8db52d3f2107c02fea13050657e46b7b540d1dab
hamdysaad20 <hamdysaad.biz@gmail.com>
2021-12-15 22:03:25 +0200

    up

--- components/contHeader.js
+++ components/contHeader.js
@@ -16,7 +16,7 @@ const navigation = [
 function contHeader() {
   return (
 
-    <header className=" mb-10">
+    <header className=" max-w-7xl mx-auto mb-10">
       <Title/>
           <div className="relative bg-white overflow-hidden  ">
       <div className="xl:ml-9 mx-auto">
@@ -101,7 +101,7 @@ function contHeader() {
             </Transition>
           </Popover>
 
-          <main className="xl:ml-9 mt-10 mx-auto max-w-screen-xl		 px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-36 lg:px-8 xl:mt-28">
+          <main className="xl:ml-20 mt-10 mx-auto max-w-screen-xl		 px-4 sm:mt-12 sm:pl-6 md:mt-16 lg:mt-36 lg:pl-8 xl:mt-28">
             <div className="text-center sm:text-center lg:text-left">
               <h1 className="text-5xl tracking-tight font-extrabold text-gray-900 sm:text-6xl md:text-8xl">
                 <span className=" block xl:inline">Contact Us</span><br/>
@@ -113,9 +113,9 @@ function contHeader() {
           </main>
         </div>
       </div>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+      <div className="lg:absolute lg:inset-y-8 z-40 lg:right-0 lg:top-2  lg:w-1/2">
 
-        <Image src="/contus.png" alt="me" layout="fill" objectFit="cover" className="h-60   sm:h-72 md:h-96 lg:w-full lg:h-full"  />
+        <Image src="/contus.png" alt="me" layout="fill" objectFit="contain" className="h-60   sm:h-72 md:h-96 lg:w-full lg:h-full"  />
 
       </div>
     </div>
