a437b78a2768dccfdd42e4bd2bad5adb2728814b
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-01-03 14:29:19 +0200

    1

---
+++ components/title.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+function title() {
+    return (
+        <div>
+ <div>
+            
+        <Head>
+        <title>Divzoon</title>
+
+          <link rel="shortcut icon" href="/divicobg.ico" />
+     </Head>      
+    </div>
+        </div>
+    )
+}
+
+export default title
