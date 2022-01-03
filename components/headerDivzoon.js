c17d07bc8228f1ae6617c1b3a25e09e390427414
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-01-03 14:44:08 +0200

    image

--- components/headerDivzoon.js
+++ components/headerDivzoon.js
@@ -2,7 +2,7 @@ import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
-import Image from 'next/Image'
+import Image from 'next/image'
 
 const navigation = [
   { name: 'Home', href: '/' },
